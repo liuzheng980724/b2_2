3376d284899d3200247dff9d1a8479ece2588da4
Leo <mail@liuzheng.org>
2022-10-03 17:39:40 +1100

    Finished auto group

--- src/b2_2/HomePageController.java
+++ src/b2_2/HomePageController.java
@@ -6,6 +6,12 @@ import java.util.List;
 public class HomePageController {
 
 	List<String[]> studentList = new ArrayList<String[]>();
+	List<String[]> classList = new ArrayList<String[]>();
+	int totalStudent = 0;
+	int minNumInGroup = 5;
+	int maxNumInGroup = 7;
+	int intMaxNumberGroups = 0;
+	int intMinNumberGroups = 0;
 	
 	public HomePageController() {
 		FileIO newFileIO = new FileIO();
@@ -18,11 +24,44 @@ public class HomePageController {
 			e.printStackTrace();
 		}
 		
+		int i = 0;
 		for(String[] students:studentList) {
 			System.out.println(students[1]);
+			i++;
+		}
+		System.out.println(i);
+		totalStudent = i;
+		caculateGroups();
+	}
+	
+	public void caculateGroups() {
+		double maxNumberGroups = totalStudent/maxNumInGroup;
+		intMaxNumberGroups = (int)maxNumberGroups;
+		int restStudents = totalStudent - (intMaxNumberGroups*maxNumInGroup);
+		double minNumberGroups = restStudents/minNumInGroup;
+		intMinNumberGroups = (int)minNumberGroups;
+		restStudents = restStudents - (intMinNumberGroups*minNumInGroup);
+		
+		int checkRestNum = restStudents/minNumInGroup;
+		
+		boolean processDone = false;
+		while(!processDone) {
+			if(checkRestNum < 1) {
+				intMaxNumberGroups = intMaxNumberGroups - 1;
+				restStudents = restStudents + maxNumInGroup;
+			}
+			double tempMinNumberGroups = restStudents/minNumInGroup;
+			int intTempMinNumberGroups = (int)tempMinNumberGroups;
+			intMinNumberGroups = intMinNumberGroups + intTempMinNumberGroups;
+			restStudents = restStudents - (intTempMinNumberGroups*minNumInGroup);
+			
+			if(restStudents == 0) {
+				processDone = true;
+			}
 		}
 		
-
-		
+		System.out.println(intMaxNumberGroups);
+		System.out.println(intMinNumberGroups);
+		System.out.println(restStudents);
 	}
 }
